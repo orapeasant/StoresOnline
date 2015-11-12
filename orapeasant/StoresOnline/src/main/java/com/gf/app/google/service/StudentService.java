dc87a4abedd27d397cdaaf0d7f61cb7e3a13a983
orapeasant <orapeasant@gmail.com>
2015-11-13 00:52:54 +0800

    init commit

--- orapeasant/StoresOnline/src/main/java/com/gf/app/google/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.gf.app.google.service;
-
-import com.gf.app.google.model.Student;
-
-public interface StudentService {
-	Student save(Student student);
-	boolean findByLogin(String userName, String password);
-	boolean findByUserName(String userName);
-}
